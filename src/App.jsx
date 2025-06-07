732e57463f3cf53bb64c8b4b4479048a18667367
Muhammad El Faritsi <daeng.faritsi@starinvestama.co.id>
2025-06-07 16:19:13 +0700

    Add humberger menu for mobile device & add profile photo for Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
 import { useState } from 'react'
 
 import './css/output.css'
+import Hamburger from 'hamburger-react'
 
 
-function BuildNavbarSection() {
+function BuildNavbarSection({ listMenu, isOpenHumbergerMenu, setHumbergerToggle }) {
   return <>
-    <div className='bg-white py-2 px-5 my-10 rounded-full md:max-w-7xl lg:flex lg:justify-between md:px-15 md:py-4 lg:mx-auto fixed -top-5 left-1/5 right-1/5 shadow-sm'>
+    <div className='bg-white py-2 pl-7 pr-4 my-10 rounded-full md:max-w-7xl flex justify-between md:pl-10 md:pr-7 md:py-4 lg:mx-auto fixed -top-5 left-1/5 right-1/5 shadow-sm items-center'>
       <div>
-        <a href="#" className='text-3xl font-bold text-emerald-600'>LOGO</a>
+        <a href="#" className='text-3xl font-bold text-emerald-600 fl'>LOGO</a>
       </div>
 
-      <div className='hidden lg:visible lg:flex lg:items-center gap-4'>
-        <a href="#" className='text-slate-700 font-poppins'>Beranda</a>
-        <a href="#" className='text-slate-700'>Tentang Saya</a>
-        <a href="#" className='text-slate-700'>Clients</a>
-        <a href="#" className='text-slate-700'>Blog</a>
-        <a href="#" className='text-slate-700'>Contact</a>
+      <div className='hidden lg:visible lg:flex gap-4'>
+        {listMenu.map((item, index) => <a href={item.url} key={index} className='text-slate-700'>{item.name}</a>)}
       </div>
+      <div className='visible lg:hidden'>
+        <div>
+          <Hamburger easing="ease-in" size={24} color="#314158" toggled={isOpenHumbergerMenu} toggle={setHumbergerToggle} />
+        </div>
+        <div className={isOpenHumbergerMenu ? 'visible' : 'hidden'}>
+          <div className='bg-white shadow-lg px-5 w-50 rounded-2xl absolute right-0 top-18 md:top-23'>
+            {listMenu.map((item, index) => <div className='my-5 text-slate-700' key={index}><a href={item.url} className=''>{item.name}</a></div>)}
+          </div>
+        </div>
+      </div>
+
     </div>
+
+
+
   </>
 }
 
+
 function BuildHeroSection() {
-  return <div className='mx-10 lg:mx-20 mt-40'>
+  return <div className='mx-10 lg:mx-20 md:mt-40 mt-30'>
     <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
       <div className='w-full row-start-2 md:row-start-1'>
         <div className='mb-10'>
@@ -35,13 +47,14 @@ function BuildHeroSection() {
         <a href="#" className='bg-emerald-600 px-6 py-2 md:py-3 rounded-full text-white font-light'>Hubungi Saya</a>
       </div>
 
-      <div className='w-full bg-blue-500'>gambar disini</div>
+      <div className='flex justify-center'>
+        <img src="src/assets/images/profile_image.jpg" alt="profile image" className='w-70 h-70 md:w-80 md:h-80 lg:w-100 lg:h-100 bg-contain rounded-full border-4 border-white shadow-lg' />
+      </div>
     </div>
   </div>
 }
 
-function BuildAboutSection() {
-  const items = ["F", "T", "I", "L"];
+function BuildAboutSection({ socialMedia }) {
   return <div className='mx-10 lg:mx-20 mt-20'>
     <h1 className='text-emerald-600 font-semibold'>TENTANG SAYA</h1>
     <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
@@ -53,7 +66,7 @@ function BuildAboutSection() {
         <h1 className='text-2xl font-semibold text-slate-700'>Mari Berteman</h1>
         <p className='text-slate-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque sint libero, iste consequatur ipsum</p>
         <div className='flex gap-3 mt-3'>
-          {items.map((items, index) => <CustomSocialMediaIcon icon={items} key={index} />
+          {socialMedia.map((items, index) => <CustomSocialMediaIcon icon={items} key={index} />
           )}
         </div>
 
@@ -70,29 +83,8 @@ function CustomSocialMediaIcon({ icon }) {
 
 }
 
-function PortfolioSection() {
-  let project = [
-    {
-      "name": "Project 1",
-      "desc": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates, amet.",
-      "image": ""
-    },
-    {
-      "name": "Project 2",
-      "desc": "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-      "image": ""
-    },
-    {
-      "name": "Project 3",
-      "desc": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis!",
-      "image": ""
-    },
-    {
-      "name": "Project 4",
-      "desc": "Lorem ipsum dolor sit amet consectetur adipisicing.",
-      "image": ""
-    }
-  ];
+function PortfolioSection({ projectList }) {
+
   return <div className='mt-20 bg-white py-5  rounded-2xl md:rounded-none'>
     <div>
       <h2 className='flex justify-center font-semibold text-emerald-600 text-xl mt-8'>Portfolio</h2>
@@ -102,7 +94,7 @@ function PortfolioSection() {
     </div>
     <div className='flex justify-center mt-7'>
       <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3'>
-        {project.map((item, index) => <CustomPortfolioCard projectName={item.name} desc={item.desc} image={item.image} key={index} />)}
+        {projectList.map((item, index) => <CustomPortfolioCard projectName={item.name} desc={item.desc} image={item.image} key={index} />)}
       </div>
     </div>
 
@@ -123,12 +115,63 @@ function CustomPortfolioCard({ projectName, desc, image }) {
 }
 
 export default function App() {
+  const [isOpenHumbergerMenu, setIsOpenHumbergerMenu] = useState(false);
+  const navbarListMenu = [
+    {
+      name: "Beranda",
+      url: "#"
+    }, {
+      name: "Tentang Saya",
+      url: "#"
+    },
+    {
+      name: "Clients",
+      url: "#"
+    },
+    {
+      name: "Blog",
+      url: "#"
+    },
+    {
+      name: "Contact",
+      url: "#"
+    }
+  ];
+  const socialMedia = ["F", "T", "I", "L"];
+  const projectList = [
+    {
+      name: "Project 1",
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates, amet.",
+      image: ""
+    },
+    {
+      name: "Project 2",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      image: ""
+    },
+    {
+      name: "Project 3",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis!",
+      image: ""
+    },
+    {
+      name: "Project 4",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      image: ""
+    }
+  ];
+
+  const setHumbergerToggle = () => {
+    setIsOpenHumbergerMenu(!isOpenHumbergerMenu);
+  };
+
+
   return (
     <>
-      <BuildNavbarSection />
+      <BuildNavbarSection listMenu={navbarListMenu} isOpenHumbergerMenu={isOpenHumbergerMenu} setHumbergerToggle={() => setHumbergerToggle()} />
       <BuildHeroSection />
-      <BuildAboutSection />
-      <PortfolioSection />
+      <BuildAboutSection socialMedia={socialMedia} />
+      <PortfolioSection projectList={projectList} />
     </>
   )
 }
